12a389e302899bbfcbb482a0df677eec9e2054c2
ricky saunders <79845902+RickySaunders@users.noreply.github.com>
2024-11-21 15:29:48 +0000

    v1

--- app/routes.js
+++ app/routes.js
@@ -1,9 +1,7 @@
-//
-// For guidance on how to create routes see:
-// https://prototype-kit.service.gov.uk/docs/create-routes
-//
 
 const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
-// Add your routes here
+//require('./routes/routes-v7')(router);
+
+//module.exports = router
