9bf9d796f6d3de2ebd4293c8efc0b6d30fac81e5
ricky saunders <ricky.saunders@digital.justice.gov.uk>
2025-02-27 18:14:40 +0000

    v1

--- app/routes/routes-v1.js
+++ app/routes/routes-v1.js
@@ -3,6 +3,7 @@ module.exports = function (router) {
     var version = '/v1';
     
     router.post(version +'/setup', function(req, res) {
-   
+
+        res.redirect(version +'/')
         });
     }
