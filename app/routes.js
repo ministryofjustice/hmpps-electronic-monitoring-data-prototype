414d8917f77b184358ffc234797e257a8f1e72c1
ricky saunders <ricky.saunders@digital.justice.gov.uk>
2025-04-03 23:16:32 +0100

    6 versions for UR

--- app/routes.js
+++ app/routes.js
@@ -4,5 +4,6 @@ const router = govukPrototypeKit.requests.setupRouter()
 
 require('./routes/routes-v1')(router);
 require('./routes/routes-v2')(router);
-
+require('./routes/routes-v3')(router);
+require('./routes/routes-v4')(router);
 module.exports = router
